7ba35b969c543add8621e0b6b3cdab4b7114cbed
Masedie <masedie@Masedies-MacBook-Air.local>
2024-06-12 15:35:25 +0200

    His Voice

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-route for handling State objects and operations
+Route for handling State objects and operations
 """
 from flask import jsonify, abort, request
 from api.v1.views import app_views, storage
